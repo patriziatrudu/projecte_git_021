c7f34b7e0b3cf19c80d53021ad8566132d17ea22
patriziatrudu <patriziamanuela.tru.dam@insalfonscostafreda.cat>
2023-12-12 16:35:05 +0100

    primer commit

---
+++ src/main/java/com/mycompany/projecte_git_01/Projecte_git_01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projecte_git_01;
+
+/**
+ *
+ * @author patriziamanuela.tru
+ */
+public class Projecte_git_01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
