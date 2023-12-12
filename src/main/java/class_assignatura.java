56db8ed48d9d9e7f9b6e1c9591f1be39361f22d1
patriziatrudu <patriziamanuela.tru.dam@insalfonscostafreda.cat>
2023-12-12 17:50:37 +0100

    projecte 01 modificat

---
+++ src/main/java/class_assignatura.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author patriziamanuela.tru
+ */
+public class class_assignatura {
+    private final String assignatura;
+    private final int codi;
+    
+    public class_persona(String assignatura,int codi){
+        this.assignatura=assignatura;
+        this.codi=codi;
+    }
+    
+    public void saludar(){
+        System.out.println("hola,estic cursat l'assignatura de " + assignatura + " amb codi: " + codi};
+}
